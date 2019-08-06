757aa2ea10fa9ca9c80c1b4d30e866baff8dc593
John Giannelos <jgiannelos@mozilla.com>
2019-08-06 16:58:31 +0200

    Implement background script basic skeleton.
    * Use webNavigation events to trigger webextension
    * Add some basic user notification
    * Use mock data

---
+++ src/background.js
@@ -0,0 +1,39 @@
+const NOTIFICATION_ID = "ml-github-webcompat-notification";
+const CLASSIFICATION_PROBABILITY_THRESHOLD = 0.8;
+
+function fetchClassificationDetails() {
+  // TODO fetch actual classification data
+
+  const classification = {
+    label: "invalid",
+    probability: 0.9
+  };
+
+  return classification;
+}
+
+function init(details) {
+  const classification = fetchClassificationDetails();
+
+  if (classification.probability >= CLASSIFICATION_PROBABILITY_THRESHOLD) {
+    const notificationTitle = "WebCompat triaging automation";
+    const notificationMsg = `Possible ${classification.label} issue`;
+
+    browser.notifications.create(NOTIFICATION_ID, {
+      type: "basic",
+      iconUrl: browser.runtime.getURL("assets/icon.svg"),
+      title: notificationTitle,
+      message: notificationMsg
+    });
+  }
+}
+
+const isGithubFilter = {
+  url: [
+    {
+      hostContains: ".github.com"
+    }
+  ]
+};
+
+browser.webNavigation.onCommitted.addListener(init, isGithubFilter);
