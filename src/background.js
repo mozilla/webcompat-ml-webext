b6866bc9fda4c760fc2e981da77082d17c2cef54
John Giannelos <johngiannelos@gmail.com>
2019-08-22 19:27:58 +0300

    Use the real classification results endpoint.

--- src/background.js
+++ src/background.js
@@ -1,31 +1,34 @@
-const GITHUB_ISSUE_REGEX = 'github.com/webcompat/web-bugs/issues/.+';
+const GITHUB_ISSUE_REGEX = "github.com/webcompat/web-bugs/issues/.+";
 const NOTIFICATION_ID = "ml-github-webcompat-notification";
 const CLASSIFICATION_PROBABILITY_THRESHOLD = 0.8;
+const S3_BUCKET_URL =
+  "https://webcompat-ml-results.s3.eu-central-1.amazonaws.com";
 
-function fetchClassificationDetails() {
-  // TODO fetch actual classification data
-
-  const classification = {
-    label: "invalid",
-    probability: 0.9
-  };
-
-  return classification;
+async function fetchClassificationDetails(url) {
+  const issueId = url.split("/").pop();
+  const classificationUrl = `${S3_BUCKET_URL}/${issueId}.json`;
+  console.log(`Webcompat issue: ${issueId}`);
+  console.log(`Classification data URL: ${classificationUrl}`);
+  let response = await fetch(classificationUrl);
+  let data = await response.json();
+  return data;
 }
 
 function init(details) {
-  const classification = fetchClassificationDetails();
+  fetchClassificationDetails(details.url).then(function(data) {
+    console.log(`Classification data: ${JSON.stringify(data)}`);
 
-  if (classification.probability >= CLASSIFICATION_PROBABILITY_THRESHOLD) {
-    const notificationTitle = "WebCompat triaging automation";
-    const notificationMsg = `Possible ${classification.label} issue`;
+    if (data[0].probability >= CLASSIFICATION_PROBABILITY_THRESHOLD) {
+      const notificationTitle = "WebCompat triaging automation";
+      const notificationMsg = `Possible ${classification.label} issue`;
 
-    browser.notifications.create(NOTIFICATION_ID, {
-      type: "basic",
-      title: notificationTitle,
-      message: notificationMsg
-    });
-  }
+      browser.notifications.create(NOTIFICATION_ID, {
+        type: "basic",
+        title: notificationTitle,
+        message: notificationMsg
+      });
+    }
+  });
 }
 
 const isGithubFilter = {
