3c8efc6a1c0055b0e8cda9af9c55dc7301f65134
John Giannelos <jgiannelos@mozilla.com>
2019-09-10 12:32:22 +0200

    Add probability threshold at 0.7 for valid issues.

--- src/background.js
+++ src/background.js
@@ -1,5 +1,6 @@
 const GITHUB_ISSUE_REGEX = "github.com/webcompat/web-bugs/issues/.+";
 const NOTIFICATION_ID = "ml-github-webcompat-notification";
+const VALID_PROBABILITY_THRESHOLD = 0.7;
 const S3_BUCKET_URL =
   "https://webcompat-ml-results.s3.eu-central-1.amazonaws.com";
 
@@ -13,20 +14,31 @@ async function fetchClassificationDetails(url) {
   return data;
 }
 
+function getClassThreshold(proba) {
+  const validProba = 1 - proba;
+  console.log(`Valid probability: ${validProba}`);
+  const classification =
+    validProba > VALID_PROBABILITY_THRESHOLD ? "valid" : "invalid";
+  return classification;
+}
+
 function init(details) {
   fetchClassificationDetails(details.url)
     .then(function(data) {
+
       console.log(`Classification data: ${JSON.stringify(data)}`);
+      const classification = getClassThreshold(data[0].probability);
 
-      const classification = data.probability < 0.5 ? "valid" : "invalid";
-      const notificationTitle = "WebCompat triaging automation";
-      const notificationMsg = `Possible ${classification} issue`;
+      if (classification === "valid") {
+        const notificationTitle = "WebCompat triaging automation";
+        const notificationMsg = `Possible ${classification} issue`;
 
-      browser.notifications.create(NOTIFICATION_ID, {
-        type: "basic",
-        title: notificationTitle,
-        message: notificationMsg
-      });
+        browser.notifications.create(NOTIFICATION_ID, {
+          type: "basic",
+          title: notificationTitle,
+          message: notificationMsg
+        });
+      }
     })
     .catch(function(error) {
       console.log(`Couldn't fetch classification data: ${error}`);
