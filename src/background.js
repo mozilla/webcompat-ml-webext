db451c96e511a7b62464d89c952916e260dda826
John Giannelos <jgiannelos@mozilla.com>
2019-09-10 12:19:59 +0200

    Multiple fixes
    * Make URL parsing more robust
    * Catch errors when fetching data

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ const S3_BUCKET_URL =
   "https://webcompat-ml-results.s3.eu-central-1.amazonaws.com";
 
 async function fetchClassificationDetails(url) {
-  const issueId = url.split("/").pop();
+  const issueId = new URL(url).pathname.split("/").pop();
   const classificationUrl = `${S3_BUCKET_URL}/${issueId}.json`;
   console.log(`Webcompat issue: ${issueId}`);
   console.log(`Classification data URL: ${classificationUrl}`);
@@ -14,19 +14,23 @@ async function fetchClassificationDetails(url) {
 }
 
 function init(details) {
-  fetchClassificationDetails(details.url).then(function(data) {
-    console.log(`Classification data: ${JSON.stringify(data)}`);
+  fetchClassificationDetails(details.url)
+    .then(function(data) {
+      console.log(`Classification data: ${JSON.stringify(data)}`);
 
-    const classification = data.probability < 0.5 ? "valid" : "invalid";
-    const notificationTitle = "WebCompat triaging automation";
-    const notificationMsg = `Possible ${classification} issue`;
+      const classification = data.probability < 0.5 ? "valid" : "invalid";
+      const notificationTitle = "WebCompat triaging automation";
+      const notificationMsg = `Possible ${classification} issue`;
 
-    browser.notifications.create(NOTIFICATION_ID, {
-      type: "basic",
-      title: notificationTitle,
-      message: notificationMsg
+      browser.notifications.create(NOTIFICATION_ID, {
+        type: "basic",
+        title: notificationTitle,
+        message: notificationMsg
+      });
+    })
+    .catch(function(error) {
+      console.log(`Couldn't fetch classification data: ${error}`);
     });
-  });
 }
 
 const isGithubFilter = {
